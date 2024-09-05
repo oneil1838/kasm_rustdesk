a706a555ebfa978b13e4b95c626d352154602dcb
oneil1838 <32617957+oneil1838@users.noreply.github.com>
2024-09-05 08:44:50 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,13 +4,13 @@ const nextConfig = {
   env: {
     name: 'Kasm Technologies',
     description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    icon: 'https://github.com/oneil1838/kasm_rustdesk/img/logo.svg',
+    listUrl: 'https://github.com/oneil1838/kasm_rustdesk/',
+    contactUrl: 'https://github.com/oneil1838/kasm_rustdesk/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/kasm_rustdesk/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
